1a9710187f1b8de2e9691e83ff4d832864a9d4c3
felixtandian <53644132+felixtandian@users.noreply.github.com>
2025-05-25 19:48:12 +0700

    final

--- core/app.ts
+++ core/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import userRoutes from '../routes/userRoutes';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', userRoutes);
 
